a7dc207c131c7f883421c4d226c8c82b6b7728be
IamZex <dbzmm1@gmail.com>
2022-03-24 10:08:14 -0500

    Creating to use with Netlify
    Got to use those skills to pay the bills.

---
+++ drs.js
@@ -0,0 +1,43 @@
+var floatLockDate  = new Date("Mar 18, 2024 09:30:00").getTime();
+var moonLaunchDate = new Date("Sept 14, 2026 09:30:00").getTime();
+
+var floatInterval = setInterval(function(){
+
+    var now = new Date().getTime();
+    var floatDistance = floatLockDate - now;
+
+    var floatDays = Math.floor(floatDistance / 86400000);
+    var floatHours = Math.floor(floatDistance % (1000*60*60*24)/(1000*60*60));
+    var floatMinutes = Math.floor(floatDistance % (1000*60*60)/(1000*60));
+    var floatSeconds = Math.floor(floatDistance % (1000*60)/1000);
+
+    floatLock = document.getElementById("floatLock").innerHTML =
+        floatDays +" Days "+ floatHours +" Hours "
+        + floatMinutes+" Minutes "+ floatSeconds +" Seconds ";
+
+        if (floatDistance < 0) {
+            clearInterval(floatInterval)
+            document.querySelector(".floatLock").textContent = "We've locked the Free Float"
+        };
+
+},1000)
+
+var moonInterval = setInterval(function(){
+
+    var now = new Date().getTime();
+    var distance = moonLaunchDate - now;
+
+    var days = Math.floor(distance /(1000*60*60*24));
+    var hours = Math.floor(distance % (1000*60*60*24)/(1000*60*60));
+    var minutes = Math.floor(distance %(1000*60*60)/(1000*60));
+    var seconds = Math.floor(distance % (1000*60)/1000);
+
+    moonLaunch = document.getElementById("moonLaunch").innerHTML =
+    days + " Days " + hours + " Hours "+ minutes + " Minutes " + seconds +" Seconds ";
+
+    if (distance < 0) {
+        clearInterval(moonInterval)
+        document.querySelector(".moonLaunch").textContent = "Happy Launch Day!"
+    };
+},1000)
+
