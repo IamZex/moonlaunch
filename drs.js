a8be5c0af81e4ca84f5edd0f2ffcadc38247ef0d
Iamzex <dbzmm1@gmail.com>
2022-09-15 11:17:30 -0500

    New dates

--- drs.js
+++ drs.js
@@ -1,15 +1,16 @@
-var floatLockDate  = new Date("June 30, 2023 09:30:00").getTime();
-var moonLaunchDate = new Date("Sept 26, 2025 09:30:00").getTime();
+var floatLockDate  = new Date("May 8, 2023 09:30:00").getTime();
+var moonLaunchDate = new Date("March 21, 2024 09:30:00").getTime();
 /*  "Mar 18, 2024 09:30:00" original float lock date
     "Sept 14, 2026 09:30:00 original moon launch date
     "July 13, 2023 09:30:00" first update float lock
+    "June 30, 2023 09:30:00" 2nd update float lock
+    "Sept 26, 2025 09:30:00" 2nd update moon launch date
 */
 
 var floatInterval = setInterval(function(){
 
     var now = new Date().getTime();
     var floatDistance = floatLockDate - now;
-
     var floatDays = Math.floor(floatDistance / 86400000);
     var floatHours = Math.floor(floatDistance % (1000*60*60*24)/(1000*60*60));
     var floatMinutes = Math.floor(floatDistance % (1000*60*60)/(1000*60));
@@ -30,7 +31,6 @@ var moonInterval = setInterval(function(){
 
     var now = new Date().getTime();
     var distance = moonLaunchDate - now;
-
     var days = Math.floor(distance /(1000*60*60*24));
     var hours = Math.floor(distance % (1000*60*60*24)/(1000*60*60));
     var minutes = Math.floor(distance %(1000*60*60)/(1000*60));
